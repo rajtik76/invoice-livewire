5a3dc7e219ffb5b86c200ffcea346e8f4a35b114
Vladislav Rajtmajer <rajtik@gmail.com>
2024-04-28 00:23:07 +0200

    Add MonthHoursOverview.php dashboard widget

---
+++ app/Filament/Widgets/MonthHoursOverview.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\TaskHour;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class MonthHoursOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $interval = CarbonPeriod::create(now()->startOfMonth(), '1 day', now());
+        $history = collect($interval->toArray())->map(function (Carbon $date) {
+            return [
+                'date' => $date,
+                'hours' => floatval(TaskHour::where('user_id', auth()->id())
+                    ->where('date', $date)
+                    ->sum('hours')),
+            ];
+        });
+
+        return [
+            Stat::make(
+                label: 'Hours in current month',
+                value: TaskHour::where('user_id', auth()->id())
+                    ->whereBetween('date', [now()->startOfMonth(), now()->endOfMonth()])
+                    ->sum('hours')
+            )
+                ->chart($history->pluck('hours')->all())
+                ->color('success'),
+        ];
+    }
+}
