ca60dd910563e5bd76b02e8078ddc3e45ed1596e
Vladislav Rajtmajer <rajtik@gmail.com>
2025-03-06 16:25:09 +0100

    Add tests for task visibility, deactivation, and deletion
    Introduce tests to ensure users only see their own tasks. Add test cases for deactivating a task and verifying it is hidden from the list. Include a delete task test to confirm proper deletion and database updates.

--- tests/Feature/Filament/Resources/TaskResource/Pages/ListTaskTest.php
+++ tests/Feature/Filament/Resources/TaskResource/Pages/ListTaskTest.php
@@ -3,9 +3,12 @@
 use App\Filament\Resources\TaskResource\Pages\ListTasks;
 use App\Models\Task;
 use App\Models\User;
+use Filament\Tables\Actions\EditAction;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Livewire\livewire;
 
 beforeEach(function () {
@@ -69,4 +72,54 @@ it('can see also inactive tasks when active filter is turned off', function () {
         ->filterTable('active', false)
         ->assertStatus(200)
         ->assertCanSeeTableRecords($tasks, true);
+});
+
+it('can see only current user tasks', function () {
+    // Arrange
+    $currentUserTasks = Task::factory()
+        ->count(2)
+        ->recycle($this->user)
+        ->create();
+
+    $anotherUsersTasks = Task::factory()
+        ->count(3)
+        ->create();
+
+    // Act & Assert
+    livewire(ListTasks::class)
+        ->assertStatus(200)
+        ->assertCanSeeTableRecords($currentUserTasks)
+        ->assertCanNotSeeTableRecords($anotherUsersTasks);
+});
+
+it('can deactivate task', function () {
+    // Arrange
+    $task = Task::factory()
+        ->recycle($this->user)
+        ->create(['id' => 1000]);
+
+    // Act & Assert
+    livewire(ListTasks::class)
+        ->callTableBulkAction('deactivate', [$task])
+        ->assertStatus(200)
+        ->assertCanNotSeeTableRecords([$task]); // Deactivated tasks can't be seen
+
+    assertDatabaseHas('tasks', [
+        'id' => $task->id,
+        'active' => false,
+    ]);
+});
+
+it('can delete task', function () {
+    // Arrange
+    $task = Task::factory()
+        ->recycle($this->user)
+        ->create(['id' => 1000]);
+
+    // Act & Assert
+    livewire(ListTasks::class)
+        ->callTableBulkAction('delete', [$task])
+        ->assertStatus(200);
+
+    assertDatabaseCount('tasks', 0);
 });
